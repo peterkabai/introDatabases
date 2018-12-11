44d39594558f4e1e31e746d3b6aa0a1ecd14f33f
Peter <pkabai@csumb.edu>
2018-12-10 21:15:56 -0800

    Adds question number headings

--- pharmacyQueries.sql
+++ pharmacyQueries.sql
@@ -6,6 +6,7 @@
 
 -- For each doctor, show the drug manuafaturer who's 
 -- drugs are presscribed most often:
+select "-- 1";
 select doc_name, max(comp_name)
 from doctor 
 natural join prescription 
@@ -14,6 +15,7 @@ natural join drug
 group by doc_name;
 
 -- Show the average age of the patients taking each drug:
+select "-- 2";
 select trade_name, avg(age)
 from patient
 natural join prescription
@@ -21,6 +23,7 @@ natural join drugs_in_presc
 group by trade_name;
 
 -- For each drug, show which doctor has prescribed it the most:
+select "-- 3";
 select trade_name, max(doc_name)
 from drugs_in_presc
 natural join prescription
@@ -29,6 +32,7 @@ group by trade_name;
 
 -- List the pescription number for prescriptions that can’t be 
 -- filled by the pharmacy it was sent to:
+select "-- 4";
 select presc_num 
 from drugs_in_presc
 natural join prescription
@@ -40,6 +44,7 @@ natural join pharmacy
 natural join contract);
 
 -- List the patients who have the same drug prescribed by multiple doctors:
+select "-- 5";
 select pat_name, trade_name, count(*)
 from prescription
 natural join patient
